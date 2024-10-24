c30e03d2708a5086e91ebe54af4cd57c4f6ad7e7
Davi Silva <davisilvaphoto@gmail.com>
2024-10-24 16:45:34 -0300

    change: replace yarn to pnpm

---
+++ src/middleware-example.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+
+export function middleware() {
+  console.log('middleware')
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/'],
+}
